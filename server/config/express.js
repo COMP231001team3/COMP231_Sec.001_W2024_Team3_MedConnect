cc6fff4ae4fcde7978e997758979f4adaef3fe29
IuliiaChugunova <ychuguno@my.centennialcollege.ca>
2024-03-10 21:53:16 -0400

    Folder structure updated

---
+++ server/config/express.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const compression = require('compression');
+const path = require('path'); // Include the path module
+
+// Create an Express application
+const app = express();
+
+// Middleware to enable CORS
+app.use(cors());
+
+// Middleware to parse incoming request bodies
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Middleware to override HTTP methods
+app.use(methodOverride('_method'));
+
+// Middleware to compress responses
+app.use(compression());
+
+// Define the path to serve static files (e.g., frontend build files)
+const staticPath = path.join(__dirname, '../../client/build'); // Adjust the path based on your project structure
+
+// Serve static files
+app.use(express.static(staticPath));
+
+// Export the Express application
+module.exports = app;
