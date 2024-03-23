b43acd99ea6b01d47bd6a6e2db62548f22b55239
COMP231001team3 <156942922+COMP231001team3@users.noreply.github.com>
2024-03-23 14:39:43 -0400

    Update auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -4,7 +4,7 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const User = require('../models/User');
+const User = require('../models/Userrs.modeljs');
 
 // User registration
 router.post('/register', async (req, res) => {
