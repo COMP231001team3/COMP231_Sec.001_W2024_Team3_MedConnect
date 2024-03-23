379149396c07e2d086d904afb842fb9b7960a1c0
COMP231001team3 <156942922+COMP231001team3@users.noreply.github.com>
2024-03-23 12:58:00 -0400

    Create is-auth.js

---
+++ server/middleware/is-auth.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+module.exports = (req, res, next) => {
+  const authHeader = req.get('Authorization');
+  if (!authHeader) {
+    const error = new Error('Not authenticated.');
+    error.statusCode = 401;
+    throw error;
+  }
+  const token = authHeader.split(' ')[1];
+  let decodedToken;
+  try {
+    console.log(decodedToken)
+    decodedToken = jwt.verify(token,process.env.SECRET || 'secretkeyappearshere');
+  } catch (err) {
+    err.statusCode = 500;
+    throw err;
+  }
+  if (!decodedToken) {
+    const error = new Error('Not authenticated.');
+    error.statusCode = 401;
+    throw error;
+  }
+  req.userId = decodedToken.userId;
+  next();
+};
