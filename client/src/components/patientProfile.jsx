d491742c055f43a4a43a83006c897f28d6dceee4
alejandrabonotto <alebff@gmail.com>
2024-04-03 09:29:28 -0400

    add import useSate

--- client/src/components/patientProfile.jsx
+++ client/src/components/patientProfile.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import './patientProfile.css';
 import profileImage from './profile.jpg';
