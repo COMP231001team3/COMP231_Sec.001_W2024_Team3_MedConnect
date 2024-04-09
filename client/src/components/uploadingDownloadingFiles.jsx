347c37818e19e3d0506677f4e7e51e7d8f00bf1c
Bruna Fernanda <silvabruna.pro@gmail.com>
2024-04-09 12:01:52 -0400

    patient data at upload/dowload page

--- client/src/components/uploadingDownloadingFiles.jsx
+++ client/src/components/uploadingDownloadingFiles.jsx
@@ -1,10 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import "./uploadingDownloadingFiles.css";
 import profileImage from "./profile.jpg";
 import folder from "./folder.jpg";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../Contexts/authContext";
 
 function UploadingDownloadingFiles() {
+  const { currentUser } = useAuth();
+  const [patient, setPatient] = useState(null);
+  const navigate = useNavigate();
+  const [selectedCategory, setSelectedCategory] = useState("");
+
+  useEffect(() => {
+    const fetchPatientData = async () => {
+      try {
+        // Fetch patient data from the backend API using the currentUser's email
+        const response = await axios.get(
+          `http://localhost:5000/patients/email/${currentUser.email}`
+        );
+        setPatient(response.data);
+      } catch (error) {
+        console.error("Error fetching patient data:", error);
+      }
+    };
+
+    if (currentUser) {
+      fetchPatientData();
+    }
+  }, [currentUser]);
+
+  if (!patient) {
+    return <div>Loading...</div>;
+  }
+
+  const handleCategoryChange = (category) => {
+    setSelectedCategory(category);
+  };
+
+  const categories = [
+    { name: "MEDICINES", label: "MEDICINES" },
+    { name: "EXAMS", label: "EXAMS" },
+    { name: "RECIPE", label: "RECIPE" },
+    { name: "ALLERGIES", label: "ALLERGIES" },
+  ];
+
   return (
     <section className="patientProfile">
       <div className="bar">
@@ -23,36 +64,35 @@ function UploadingDownloadingFiles() {
               height="100"
               className="d-inline-block align-text-top mb-3"
             />
-            <p>Name:</p>
-            <p>Email:</p>
-            <p>Birthday</p>
-            <p>Cell:</p>
-            <p>Address:</p>
+            <p>Name: {patient.name}</p>
+            <p>Email: {patient.email}</p>
+            <p>Birthday: {patient.birth}</p>
+            <p>Cell: {patient.phone}</p>
+            <p>Address: {patient.address}</p>
           </div>
-          <div className="uploading">
-            <div className="uploadHeader">
+          <div className="files">
+            <div className="DowloadHeader">
               <h2>DOWNLOAD FILES HERE</h2>
               <button>DOWNLOAD</button>
             </div>
             <div className="folderContainer">
-              <div>
-                <img src={folder} alt="MEDICINES" width="80px" height="80" />
-                <p>MEDICINES</p>
-              </div>
-              <div>
-                <img src={folder} alt="EXAMS" width="80px" height="80" />
-                <p>EXAMS</p>
-              </div>
-              <div>
-                <img src={folder} alt="RECIPE" width="80px" height="80" />
-                <p>RECIPE</p>
-              </div>
-              <div>
-                <img src={folder} alt="ALLERGIES" width="80px" height="80" />
-                <p>ALLERGIES</p>
-              </div>
+              {categories.map((category) => (
+                <div
+                  key={category.name}
+                  onClick={() => handleCategoryChange(category.name)}
+                  className="folder"
+                >
+                  <img
+                    src={folder}
+                    alt={category.name}
+                    width="80px"
+                    height="80"
+                  />
+                  <p>{category.label}</p>
+                </div>
+              ))}
             </div>
-            <div className="downloading">
+            <div className="uploading">
               <h2>UPLOAD FILES HERE</h2>
               <button>UPLOAD</button>
             </div>
